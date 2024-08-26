db840cd67af2aa68885a61d1331f4a1f28b2703c
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2024-08-26 16:34:54 +0200

    scripts: add zstd support for easy netcat helper

--- scripts/easy_data_nc_rx.sh
+++ scripts/easy_data_nc_rx.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-2.0
 
-nc -q 5 "${1}" 12345 | \
-tar --extract --file - --preserve-permissions --preserve-order \
-    --xattrs-include='*.*' --numeric-owner --directory "${2}"
+function __tar_extract {
+  tar --extract --file - --preserve-permissions --preserve-order \
+    --xattrs-include='*.*' --numeric-owner --directory="${1}"
+}
+
+function easy_data_nc_rx {
+  local use_zstd=0
+
+  if [[ "${1}" == "--zstd" ]]; then
+    use_zstd=1
+    shift
+  fi
+
+  if [[ ${use_zstd} -eq 1 ]]; then
+    nc -q 5 "${1}" 12345 | zstd --decompress --stdout | pv | __tar_extract "${2}"
+  else
+    nc -q 5 "${1}" 12345 | pv | __tar_extract "${2}"
+  fi
+}
--- scripts/easy_data_nc_tx.sh
+++ scripts/easy_data_nc_tx.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-2.0
 
-tar --create --file - --preserve-permissions --xattrs-include='*.*' \
-    --numeric-owner --directory "${1}" ./ | pv | nc -q 5 -l -p 12345
+function __tar_create {
+  tar --create --file - --preserve-permissions --xattrs-include='*.*' \
+    --numeric-owner --directory="${1}" ./
+}
+
+function easy_data_nc_tx {
+  local use_zstd=0
+
+  if [[ "${1}" == "--zstd" ]]; then
+    use_zstd=1
+    shift
+  fi
+
+  if [[ ${use_zstd} -eq 1 ]]; then
+    __tar_create "${1}" | zstd --compress --stdout -9 | pv | nc -q 5 -l -p 12345
+  else
+    __tar_create "${1}" | pv | nc -q 5 -l -p 12345
+  fi
+}
