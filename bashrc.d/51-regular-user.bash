39ccb313387eeab8a9b908808313cebb24c1ff6d
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2024-08-28 21:02:02 +0200

    bashrc.d: add functions from onyx

--- bashrc.d/51-regular-user.bash
+++ bashrc.d/51-regular-user.bash
@@ -1,3 +1,27 @@
+function __vnc_internal {
+  local vnc_host
+  local vnc_username
+  local vnc_password
+
+  vnc_host="${1}"
+  vnc_username="${2}"
+  vnc_password="${3}"
+
+  case "${1}" in
+    "--spawn" )
+      ssh -L 15900:localhost:5900 "${vnc_host}" "env WAYLAND_DISPLAY=wayland-1 wayvnc --keyboard=de-nodeadkeys --render-cursor --gpu 127.0.0.1 5900" ;;
+
+    "--connect" )
+      env VNC_USERNAME="${vnc_username}" VNC_PASSWORD="${vnc_password}" vncviewer localhost::15900 ;;
+
+    "--exit" )
+      ssh "${vnc_host}" "wayvncctl wayvnc-exit" ;;
+
+    * )
+      echo "Usage: ${FUNCNAME} --spawn|--connect|--exit" ;;
+  esac
+}
+
 function audioserver_bs2b {
   local profile
   local cmd
@@ -99,6 +123,100 @@ function dtbash {
   else
     dtach -A "${session}" /bin/bash
   fi
+}
+
+function envconfig {
+  case "${1}" in
+    "wine" )
+      export WINEPREFIX="${HOME}/.wine64"
+      export WINEARCH=win64 ;;
+
+    * )
+      echo "Usage: ${FUNCNAME} wine"
+      return 1
+  esac
+}
+
+function launch_game {
+  local target_game
+  local gameargs
+  local hlargs="-console -nojoy -noipx -novid -noextracds -lw 1920 -lh 1080 -32bpp -freq 144"
+
+  target_game="${1}"
+  shift
+
+  case "${target_game}" in
+    "--cstrike" )
+      if [[ -z "${2}" ]]; then
+        echo "error: missing server IP argument"
+
+        return 1
+      fi
+
+      gameargs="-game cstrike +exec adminpass.cfg +connect ${2}"
+
+      schedtool -a 0 -e \
+      wine "C:\Games\Half-Life\Client\hl.exe" ${hlargs} ${gameargs} ;;
+
+    "--deusex" )
+      # Deus Ex has issues with multi-core.
+      schedtool -a 0 -e \
+      wine "C:\Games\Deus Ex GOTY\System\DeusEx.exe" -localdata -skipdialog ;;
+
+    "--hl" )
+      setxkbmap -layout us
+
+      schedtool -a 0 -e \
+      wine "C:\Games\Half-Life\Client\hl.exe" ${hlargs}
+
+      setxkbmap -layout de ;;
+
+    "--l4d2" )
+      DXVK_CONFIG_FILE="${HOME}/local/dxvk-l4d2.conf" \
+      wine start /d "C:\Games\Left 4 Dead 2" "steamclient_loader.exe" ;;
+
+    "--re2" )
+      DXVK_CONFIG_FILE="${HOME}/local/dxvk/re2.conf" \
+      wine start /d "D:\Games\Resident Evil 2 2019" "re2.exe" ;;
+
+    "--ssam2" )
+      wine "C:\Games\Serious Sam 2\Bin\Sam2.exe" ;;
+
+    "--ssamtfe" )
+      # Serious Sam: The First Encounter has issues with multi-core.
+      # Also force TCP nodelay for any sockets.
+      SSAMTFE_NETCFG=1 \
+      LD_PRELOAD=${HOME}/local/lib/libnodelay.so \
+      schedtool -a 0 -e \
+      wine "C:\Program Files\Croteam\Serious Sam\Bin\SeriousSam.exe" ;;
+
+    "--ssamtfe-dedicated" )
+      # Dedicated Serious Sam: TFE server
+      SSAMTFE_NETCFG=1 \
+      LD_PRELOAD=${HOME}/local/lib/libnodelay.so \
+      schedtool -a 0 -e \
+      wine "C:\Program Files\Croteam\Serious Sam\Bin\DedicatedServer.exe" "$@" ;;
+
+    "--ssamtse" )
+      wine "C:\Program Files\Croteam\Serious Sam - The Second Encounter\Bin\SeriousSam.exe" ;;
+
+    "--ut99" )
+      PULSE_SERVER=audioserver ${HOME}/HostSystem/Games/UnrealTournament/System/ut-bin ;;
+
+    * )
+      echo "Usage: ${FUNCNAME}"
+      echo -e "\t --cstrike <server IP>"
+      echo -e "\t --deusex [Deus Ex]"
+      echo -e "\t --hl [Half-Life (standalone)]"
+      echo -e "\t --l4d2 [Left 4 Dead 2]"
+      echo -e "\t --re2 [Resident Evil 2 (2019)]"
+      echo -e "\t --ssam2 [Serious Sam 2]"
+      echo -e "\t --ssamtfe [Serious Sam: The First Encounter]"
+      echo -e "\t --ssamtfe-dedicated [Serious Sam dedicated server]"
+      echo -e "\t --ssamtse [Serious Sam: The Second Encounter]"
+      echo -e "\t --ut99"
+       ;;
+  esac
 }
 
 function permission_sanitize {
